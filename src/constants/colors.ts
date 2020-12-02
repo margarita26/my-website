c0a7725c3e0b83e06aa9b30a0c6596ccaa831f6e
Margarita Gubanova <gubanova.marg@yandex.com>
2020-12-01 19:06:09 -0500

    test for s3 bucket

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,8 +1,9 @@
 export const colors = {
-  white: 'white',
-  black: 'black',
-  grey: 'grey',
-  lightGrey: '#d3d3d3',
-  neonBlue: '#55F6F6',
-  primary: '#0B1A2E',
+  white: "white",
+  black: "black",
+  grey: "grey",
+  lightGrey: "#d3d3d3",
+  neonBlue: "#55F6F6",
+  primary: "#0B1A2E",
+  transparent: "transparent",
 };
